4b26fdc916cce563ca9a09426c9e49534ceea095
Vikram Chatterjee <strycora@gmail.com>
2021-03-01 17:13:48 -0700

    added main, resetMain, and addPlaceLink to js

--- frontend/index.js
+++ frontend/index.js
@@ -2,3 +2,14 @@ let places = []
 
 const baseUrl = "http://localhost:3000"
 
+function main(){
+  return document.getElementById("main");
+}
+
+function resetMain(){
+  main().innerHTML = "";
+}
+
+function addPlaceLink(){
+  return document.getElementById("add-place")
+}

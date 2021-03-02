5999f4b0fbab7a4460dee097b5b8ef6413b7002c
Vikram Chatterjee <strycora@gmail.com>
2021-03-01 17:22:14 -0700

    created formTemplate to add place

--- frontend/index.js
+++ frontend/index.js
@@ -13,3 +13,21 @@ function resetMain(){
 function formLink(){
   return document.getElementById("form-link")
 }
+
+function formTemplate(){
+  return `
+  <h3>Add a Favorite Place</h3>
+  <form id="form">
+    <div class="input-field">
+      <label for="name">Name</label>
+      <input type="text" name="name" id="name" />
+    </div>
+
+    <div class="input-field">
+      <label for="description">Description</label><br />
+      <textarea name="description" id="description" cols="30" rows="10"></textarea>
+    </div>
+    <input type="submit" value="Add Place" />
+  </form>
+  `;
+}

2fe8902bc99ef63c9c6bea74f2cb7f199fc7f659
Vikram Chatterjee <strycora@gmail.com>
2021-03-05 16:15:37 -0700

    added comments resource to backend

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_235223) do
+ActiveRecord::Schema.define(version: 2021_03_05_230854) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.integer "place_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["place_id"], name: "index_comments_on_place_id"
+  end
 
   create_table "places", force: :cascade do |t|
     t.string "name"
@@ -19,4 +27,5 @@ ActiveRecord::Schema.define(version: 2021_03_02_235223) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "places"
 end
